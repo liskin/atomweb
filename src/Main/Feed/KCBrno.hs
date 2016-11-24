50c0f1f0bff0d1a4af875c735d8ee8cd44064c55
Tomas Janousek <tomi@nomi.cz>
2016-11-24 21:50:20 +0100

    Small cleanup

--- src/Main/Feed/KCBrno.hs
+++ src/Main/Feed/KCBrno.hs
@@ -7,9 +7,8 @@ module Main.Feed.KCBrno (kcBrnoDiskuzeFeed) where
 import Control.Applicative ((<$>), pure)
 import Data.Bool (Bool(True))
 import Data.Function ((.), ($))
-import Data.List (head)
 import Data.Maybe (Maybe(Just))
-import Data.Monoid ((<>))
+import Data.Monoid ((<>), mconcat)
 import Data.String (String)
 import System.IO (IO)
 
@@ -18,9 +17,10 @@ import Data.Time.Format (parseTimeOrError, defaultTimeLocale)
 import qualified Data.Text.Lazy as Text (Text, unpack, stripPrefix)
 import qualified Data.Text.Lazy.Encoding as Text (decodeUtf8)
 import qualified Network.Wreq as Wreq (get, responseBody)
-import qualified Text.HTML.TagSoup as TS (Tag(TagText))
+import qualified Text.HTML.TagSoup as TS (innerText)
 import qualified Text.HTML.TagSoup.Tree as TS
-    ( TagTree(TagBranch, TagLeaf)
+    ( TagTree(TagBranch)
+    , flattenTree
     , parseTree
     , renderTree
     , transformTree
@@ -44,13 +44,15 @@ kcBrnoDiskuzeFeed = do
         TS.TagBranch "div" [("class", "prispevek")] prispevek <- TS.universeTree tree
         let date = getDate prispevek
         let atomDate = toAtomDate date
-        pure
-            $ mkAtomEntry ("http://www.kcbrno.org/diskuze.php#date_" <> atomDate) date atomDate
-            $ mkAtomHtml $ Text.unpack $ TS.renderTree $ fixLinks prispevek
+        let entryUri = "http://www.kcbrno.org/diskuze.php#date_" <> atomDate
+        let entryTitle = date
+        let entryContent = mkAtomHtml $ Text.unpack $ TS.renderTree $ fixLinks prispevek
+        pure $ mkAtomEntry entryUri entryTitle atomDate entryContent
   where
     getDate p = Text.unpack date
       where
-        date = head [ d | TS.TagBranch "div" [("class", "datum")] [TS.TagLeaf (TS.TagText d)] <- TS.universeTree p ]
+        date = TS.innerText $ mconcat
+            [ TS.flattenTree ts | TS.TagBranch "div" [("class", "datum")] ts <- TS.universeTree p ]
 
     toAtomDate = mkAtomDate . parseTimeOrError True defaultTimeLocale "%Y-%m-%d %H:%M:%S"
 
